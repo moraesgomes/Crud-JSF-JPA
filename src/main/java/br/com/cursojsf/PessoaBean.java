6e8a3f6dfaaeb6a1cb327373b3aa5197fe8642d0
haroldo gomes <moraesgomes@gmail.com>
2022-12-20 16:57:45 -0300

    Criando mensagens para os usuários após alterações realizadas.

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
@@ -28,9 +29,19 @@ public class PessoaBean  {
 		
 		pessoa = daoGeneric.merge(pessoa);
 		carregarPessoas();
+		
+	    mostrarMsg("Cadastrado com sucesso !!!");
 		return "";
 	}
 	
+	private void mostrarMsg(String msg) {
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		FacesMessage message = new FacesMessage(msg);
+		context.addMessage(null, message);
+		
+	}
+
 	public String novo() {
 		
 		pessoa = new Pessoa();
@@ -43,6 +54,7 @@ public class PessoaBean  {
 		daoGeneric.deletePorID(pessoa);
 		pessoa = new Pessoa();
 		carregarPessoas();
+		mostrarMsg("Removido com sucesso !!!");
 		return "";
 	}
 	
