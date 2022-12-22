b13770434d4124f94f119f083f89e7653b48ad92
haroldo gomes <moraesgomes@gmail.com>
2022-12-22 17:46:03 -0300

    consulta cep com web service

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,5 +1,10 @@
 package br.com.cursojsf;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +14,10 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 
 import br.com.dao.DaoGeneric;
 import br.com.entidades.Pessoa;
@@ -120,4 +129,38 @@ public class PessoaBean  {
 		return pessoaUser.getPerfilUser().equals(acesso);
 	}
 	
+	public void pesquisarCep(AjaxBehaviorEvent event) {
+		
+		try {
+			
+			URL url = new URL("https://viacep.com.br/ws/"+pessoa.getCep()+"/json/");
+			URLConnection connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+			
+			String cep = "";
+			StringBuilder jsonCep = new StringBuilder();
+			
+			while((cep = br.readLine()) != null) {
+				
+				jsonCep.append(cep);
+			}
+			
+			Pessoa gsonAux = new Gson().fromJson(jsonCep.toString(),Pessoa.class);
+			
+			pessoa.setCep(gsonAux.getCep());
+			pessoa.setLogradouro(gsonAux.getLogradouro());
+			pessoa.setComplemento(gsonAux.getComplemento());
+			pessoa.setBairro(gsonAux.getBairro());
+			pessoa.setLocalidade(gsonAux.getLocalidade());
+			pessoa.setUf(gsonAux.getUf());
+			pessoa.setUnidade(gsonAux.getUnidade());
+			pessoa.setGia(gsonAux.getGia());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			mostrarMsg("Erro ao consultar o cep");
+		}
+		
+	}
 }
--- src/main/java/br/com/entidades/Pessoa.java
+++ src/main/java/br/com/entidades/Pessoa.java
@@ -13,119 +13,193 @@ import javax.persistence.TemporalType;
 
 @Entity
 public class Pessoa implements Serializable {
-	
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	private String nome;
-	
+
 	private String sobrenome;
-	
+
 	private Integer idade;
-	
+
 	private String[] frameworks;
-	
+
 	private Boolean ativo;
-	
+
 	private String login;
-	
+
 	private String senha;
-	
+
 	private String nivelProgramador;
-	
+
 	private Integer[] linguagens;
-	
+
 	private String perfilUser;
-	
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento = new Date();
-	
+
 	private String sexo;
-	
-	
-    public Pessoa() {
-		
+
+	private String cep;
+
+	private String logradouro;
+
+	private String complemento;
+
+	private String bairro;
+
+	private String localidade;
+
+	private String uf;
+
+	private String unidade;
+
+	private String ibge;
+
+	private String gia;
+
+	public Pessoa() {
+
 	}
-    
-    public void setLinguagens(Integer[] linguagens) {
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+
+	public String getIbge() {
+		return ibge;
+	}
+
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
+
+	public String getGia() {
+		return gia;
+	}
+
+	public void setGia(String gia) {
+		this.gia = gia;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setLinguagens(Integer[] linguagens) {
 		this.linguagens = linguagens;
 	}
-    
-    public Integer[] getLinguagens() {
+
+	public Integer[] getLinguagens() {
 		return linguagens;
 	}
-    
-   
-    public void setNivelProgramador(String nivelProgramador) {
+
+	public void setNivelProgramador(String nivelProgramador) {
 		this.nivelProgramador = nivelProgramador;
 	}
-    
-    public String getNivelProgramador() {
+
+	public String getNivelProgramador() {
 		return nivelProgramador;
 	}
-    
-    public void setPerfilUser(String perfilUser) {
+
+	public void setPerfilUser(String perfilUser) {
 		this.perfilUser = perfilUser;
 	}
-    
-    public String getPerfilUser() {
+
+	public String getPerfilUser() {
 		return perfilUser;
 	}
-    
-    
-    
-    public String getLogin() {
+
+	public String getLogin() {
 		return login;
 	}
-
-
 
 	public void setLogin(String login) {
 		this.login = login;
 	}
 
-
-
 	public String getSenha() {
 		return senha;
 	}
-
-
 
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
 
-
-
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
 	}
-    
-    
-    
-    public Boolean getAtivo() {
+
+	public Boolean getAtivo() {
 		return ativo;
 	}
 
 	public void setFrameworks(String[] frameworks) {
 		this.frameworks = frameworks;
 	}
-    
-    public String[] getFrameworks() {
+
+	public String[] getFrameworks() {
 		return frameworks;
 	}
-    
-    public void setSexo(String sexo) {
+
+	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-    
-    public String getSexo() {
+
+	public String getSexo() {
 		return sexo;
 	}
 
@@ -185,8 +259,5 @@ public class Pessoa implements Serializable {
 		Pessoa other = (Pessoa) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-
 
 }
